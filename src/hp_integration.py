b3121d7029a3b60b0f25bb66d0f40630636ff7ac
Julian.Endres <julian.endres@rl-institut.de>
2022-09-30 01:04:44 +0200

    Rework hp_integration

--- src/hp_integration.py
+++ src/hp_integration.py
@@ -2,7 +2,7 @@
 import os
 
 from pathlib import Path
-import datetime
+from datetime import datetime
 import edisgo.opf.lopf as lopf
 import pandas as pd
 
@@ -19,17 +19,32 @@ from db_data import (
     get_random_residential_buildings,
 )
 from model_solving import get_downstream_matrix
-from tools import get_config, setup_logfile
+from tools import get_config, setup_logfile, create_dir_or_variant
+
+from data import __path__ as data_dir
+from results import __path__ as results_dir
+
+
+data_dir = data_dir[0]
+results_dir = results_dir[0]
 
 engine = db.engine()
 
 
 def create_heatpumps_from_db(edisgo_obj):
+    """"""
 
-    cfg_m = get_config()["model"]
+    def check_nans(df):
+        """Checks for any NaN values"""
+        if any(df.isna().any(axis=0)):
+            nan_building_ids = df.columns[df.isna().any(axis=0).values]
+            raise ValueError(
+                f"There are NaN-Values in the following buildings: "
+                f"{nan_building_ids}"
+            )
 
     # HP-disaggregation
-    # TODO insert here
+    # TODO HP-disaggregation insert here
 
     # Workaround: assign to half of all residentials
     # Get all residentials
@@ -40,33 +55,24 @@ def create_heatpumps_from_db(edisgo_obj):
 
     # Get random residential buildings from DB
     db_building_ids = get_random_residential_buildings(
-        scenario="eGon2035", limit=number_of_hps
-    )["building_id"].tolist()
+        scenario="eGon2035", limit=number_of_hps)["building_id"].tolist()
 
     # Select random residential loads
     residential_loads = residential_loads.sample(number_of_hps)
-    # Create HP names for selected residential loads
-    hp_names = [f"HP_{i}" for i in residential_loads.index]
     buses = residential_loads.bus
 
-    # Map residential loads to db building id randomly
+    # Create HP names and map to db building id randomly
+    hp_names = [f"HP_{i}" for i in residential_loads.index]
     map_hp_to_loads = dict(zip(db_building_ids, hp_names))
 
     # Get cop for selected buildings
     cop_df = get_cop(db_building_ids)
-    # if any nan value in ts raise error
-    if any(cop_df.isna().any(axis=0)):
-        nan_building_ids = cop_df.columns[cop_df.isna().any(axis=0).values]
-        raise ValueError(
-            f"There are NaN-Values in the following cop_df of buildings: {nan_building_ids}"
-        )
-
-    # Rename ts for residential buildings
+    check_nans(cop_df)
     cop_df = cop_df.rename(columns=map_hp_to_loads)
 
-    # Get heat timeseries for selected buildings
     # TODO get heat_time_series for all buildings in MVGD
     #  and remove district heating buildings
+    # Get heat timeseries for selected buildings
     heat_demand_df = pd.concat(
         [
             create_timeseries_for_building(building_id, scenario="eGon2035")
@@ -74,45 +80,35 @@ def create_heatpumps_from_db(edisgo_obj):
         ],
         axis=1,
     )
-
-    # if any nan value in ts raise error
-    if any(heat_demand_df.isna().any(axis=0)):
-        nan_building_ids = heat_demand_df.columns[
-            heat_demand_df.isna().any(axis=0).values
-        ]
-        raise ValueError(
-            f"There are NaN-Values in the following heat_demand_df of buildings: {nan_building_ids}"
-        )
-
-    # Rename ts for residential buildings
+    check_nans(heat_demand_df)
     heat_demand_df = heat_demand_df.rename(columns=map_hp_to_loads)
 
-    # Workaround: generate random heat time series
-    # heat_demand_df = pd.DataFrame(np.random.rand(8760, number_of_hps) /1e3, columns=hp_names)
-
-    # TODO adapt timeindex
+    # Adapt timeindex to timeseries
     year = edisgo_obj.timeseries.timeindex.year.unique()[0]
-
     timeindex_db = pd.date_range(
-        start=f"{year}-01-01 00:00:00", end=f"{year}-12-31 23:45:00", freq="h"
+        start=f"{year}-01-01 00:00:00",
+        end=f"{year}-12-31 23:45:00",
+        freq="h"
     )
     heat_demand_df.index = timeindex_db
     cop_df.index = timeindex_db
 
-    # Resample COP and demand
+    # Resample COP and demand if frequencies are not equal
     freq_load = pd.Series(edisgo_obj.timeseries.timeindex).diff().min()
     if not freq_load == timeindex_db.freq:
         heat_demand_df = heat_demand_df.resample(freq_load).ffill()
         cop_df = cop_df.resample(freq_load).ffill()
         logger.info(
-            f"Heat demand ts and cop ts resampled to from {timeindex_db.freq} to {freq_load}"
+            f"Heat demand ts and cop ts resampled to from "
+            f"{timeindex_db.freq} to {freq_load}"
         )
 
-    # Select keep loads timesteps
+    # Only keep loads timesteps
     heat_demand_df = heat_demand_df.loc[edisgo_obj.timeseries.timeindex]
     cop_df = cop_df.loc[edisgo_obj.timeseries.timeindex]
-    logger.info(f"Heat pump time series adapted to year {year}")
+    logger.info(f"Heat pump time series cut adapted to year {year}")
 
+    # TODO set p_set? minimal capacity?
     hp_p_set = heat_demand_df.div(cop_df).max() * 0.8
 
     loads_df = pd.DataFrame(
@@ -120,8 +116,6 @@ def create_heatpumps_from_db(edisgo_obj):
         columns=["bus", "p_set", "type"],
         data={"bus": buses.values, "p_set": hp_p_set, "type": "heat_pump"},
     )
-    # TODO p_set - peak heat demand
-    #   script von birgit? minimum_hp_capacity
 
     thermal_storage_units_df = pd.DataFrame(
         data={
@@ -140,39 +134,19 @@ def create_heatpumps_from_db(edisgo_obj):
         keys=["residential_buildilng_id", "db_building_id", "hp_building_id"],
         axis=1,
     ).droplevel(1, axis=1)
+
+    # Add to edisgo obj
     edisgo_obj.heat_pump.heat_demand_df = heat_demand_df
     edisgo_obj.heat_pump.cop_df = cop_df
     edisgo_obj.heat_pump.thermal_storage_units_df = thermal_storage_units_df
 
-    edisgo_obj.topology.loads_df = pd.concat([edisgo_obj.topology.loads_df, loads_df])
-    logger.info(
-        f"{sum(loads_df.p_set):.2f} MW of heat pumps for individual heating integrated."
+    edisgo_obj.topology.loads_df = pd.concat(
+        [edisgo_obj.topology.loads_df, loads_df]
     )
-
-    return edisgo_obj
-
-
-# @logger.catch
-def build_model(cfg):
-    """
-    Builds the model.
-    """
-    logger.info("Build model")
-    cfg_m = cfg["model"]
-    logger.info(f"Model settings:{cfg_m}")
-
-    # import Grid
-    import_dir = Path(cfg_m["feeder-dir"]) / Path(cfg_m["feeder-id"])
-    edisgo_obj = import_edisgo_from_files(import_dir, import_timeseries=True)
-    logger.info(f"eDisGo object imported: {cfg_m['grid-id']}/{cfg_m['feeder-id']}")
-
-    # Resampling not possible as no coherent timeseries
-    # edisgo_obj.timeseries.resample("h")
-    # logger.info("Resampled Time series to h")
-
-    # Add heatpumps fron egon-data-db
-    edisgo_obj = create_heatpumps_from_db(edisgo_obj)
-    logger.info("Added heat pumps to eDisGo")
+    logger.info(
+        f"{sum(loads_df.p_set):.2f} MW of heat pumps for individual "
+        f"heating integrated."
+    )
 
     return edisgo_obj
 
@@ -181,13 +155,14 @@ def write_metadata(path, edisgo_obj):
     """"""
 
     # TODO generate metadat from edisgo_obj
+    # copy from  check_integrity
     # edisgo_obj
     metadata = [f"This is Delhi \n", "This is Paris \n", "This is London \n"]
-
+    grid_id = edisgo_obj.topology.mv_grid
     # Writing to file
     with open(Path(f"{path}/metadata.md"), "w") as file:
         # Writing data to a file
-        file.write(f"METADATA for Grid {cfg_m['grid-id']} \n {'*'*20} \n \n")
+        file.write(f"METADATA for Grid {grid_id} \n {'*'*20} \n \n")
         file.writelines(metadata)
 
 
@@ -197,20 +172,46 @@ if __name__ == "__main__":
     setup_logfile(cfg)
 
     cfg_m = cfg["model"]
+
+    feeder = cfg_m["feeder"]
+    grid_id = cfg_m["grid-id"]
+    feeder_id = cfg_m["feeder-id"]
     # setup_directory(cfg_m)
-    edisgo_obj = build_model(cfg)
-    logger.info("Model is build")
 
-    export_path = Path(
-        f"{cfg_m['working-dir']}/{cfg_m['grid-id']}/{cfg_m['feeder-id']}"
-    )
+    logger.info("Build model")
+    logger.info(f"Model settings:{cfg_m}")
 
-    if os.path.isdir(export_path):
-        run_id = datetime.now().strftime("run_%Y-%m-%d-%H-%M-%S")
-        export_path = export_path / Path(f"{run_id}")
-        os.makedirs(export_path, exist_ok=True)
+    # import Grid
+    if feeder:
+        import_dir = os.path.join(results_dir,
+                                  f"{grid_id}/{feeder_id}")
     else:
-        os.makedirs(export_path, exist_ok=True)
+        import_dir = os.path.join(results_dir,
+                                  f"edisgo_objects_emob/{grid_id}")
+
+    edisgo_obj = import_edisgo_from_files(import_dir,
+                                          import_topology=True,
+                                          import_timeseries=True,
+                                          import_electromobility=True,
+                                          )
+
+    logger.info(f"eDisGo object imported: {grid_id}/{feeder_id}")
+
+    # Add heatpumps fron egon-data-db
+    edisgo_obj = create_heatpumps_from_db(edisgo_obj)
+    logger.info("Added heat pumps to eDisGo")
+
+    if feeder:
+        export_path = os.path.join(results_dir,
+                                   f"edisgo_objects_emob_hp"
+                                   f"/{grid_id}"
+                                   f"/{feeder_id}")
+    else:
+        export_path = os.path.join(results_dir,
+                                   f"edisgo_objects_emob_hp"
+                                   f"/{grid_id}")
+
+    create_dir_or_variant(export_path)
 
     edisgo_obj.save(
         directory=export_path,
@@ -222,50 +223,6 @@ if __name__ == "__main__":
     )
     logger.info(f"Model saved to: {export_path}")
 
-    write_metadata(export_path, edisgo_obj)
     # TODO write metadata
+    write_metadata(export_path, edisgo_obj)
 
-    # # Due to different voltage levels, impedances need to adapted
-    # # TODO alternatively p.u.
-    # edisgo_obj = convert_impedances_to_mv(edisgo_obj)
-    # logger.info("Converted impedances to mv")
-    #
-    # import_dir = Path(cfg_m["feeder-dir"]) / Path(cfg_m["feeder-id"])
-    # downstream_nodes_matrix = get_downstream_matrix(import_dir, edisgo_obj)
-    # logger.info("Downstream node matrix imported")
-    #
-    # # Create dict with time invariant parameters
-    # parameters = lopf.prepare_time_invariant_parameters(
-    #     edisgo_obj,
-    #     downstream_nodes_matrix,
-    #     pu=False,
-    #     optimize_storage=False,
-    #     optimize_ev_charging=False,
-    #     optimize_hp=True,
-    # )
-    # logger.info("Time-invariant parameters extracted")
-    #
-    # # cfg_o = get_config()["opt"]
-    #
-    # timesteps = list(range(24))
-    # timesteps = edisgo_obj.timeseries.timeindex[timesteps]
-    # model = lopf.setup_model(
-    #     parameters,
-    #     timesteps=timesteps,
-    #     objective="residual_load",
-    # )
-    # logger.info("Model is setup")
-    #
-    # logger.info("Optimize model")
-    # results = lopf.optimize(model, "gurobi")
-    #
-    # # existing_loggers = [logging.getLogger()]  # get the root logger
-    # # existing_loggers = existing_loggers + [
-    # #     logging.getLogger(name) for name in logging.root.manager.loggerDict
-    # # ]
-    # #
-    # # for logger in existing_loggers:
-    # #     print(logger)
-    # #     print(logger.handlers)
-    # #     print("-"*20)
-    # logger.info("Model solved")
