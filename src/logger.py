4e783ce09b3bf3a0f643f030a31eff3ac7b4f976
Julian.Endres <julian.endres@rl-institut.de>
2022-10-25 19:16:12 +0200

    Fix logs dir3

--- src/logger.py
+++ src/logger.py
@@ -5,9 +5,7 @@ from pathlib import Path
 
 from loguru import logger
 
-from logs import __path__ as logs_dir
-
-logs_dir = Path(logs_dir[0])
+logs_dir = Path("/home/local/RL-INSTITUT/julian.endres/Projekte/eDisGo-lobaflex/logs")
 
 os.makedirs(logs_dir, exist_ok=True)
 # logger.remove()
