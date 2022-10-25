456b5a4a5c6a0bc7ba00bbc04303c56fb8c795f5
Julian.Endres <julian.endres@rl-institut.de>
2022-10-25 19:12:51 +0200

    Fix logs dir2

--- src/__init__.py
+++ src/__init__.py
@@ -7,9 +7,6 @@ from loguru import logger
 
 logs_dir = Path("/home/local/RL-INSTITUT/julian.endres/Projekte/eDisGo-lobaflex/logs")
 
-
-logs_dir = Path(logs_dir[0])
-
 os.makedirs(logs_dir, exist_ok=True)
 # logger.remove()
 logfile = logs_dir / f"{date.isoformat(date.today())}.log"
