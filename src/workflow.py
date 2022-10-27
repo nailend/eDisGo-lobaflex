de19cb2c34b218ee96472f19328368f9607b092e
Julian.Endres <julian.endres@rl-institut.de>
2022-10-27 18:08:49 +0200

    Resample after emob to 1h

--- src/workflow.py
+++ src/workflow.py
@@ -3,15 +3,22 @@
 from emob_integration import run_emob_integration
 from hp_integration import run_hp_integration
 from load_integration import run_load_integration
-
+from logger import logger
 
 # load generation
+logger.info("Start load integration")
 edisgo_obj = run_load_integration(save=True)
 
 # emob integration
+logger.info("Start emob integration")
 edisgo_obj, flex_bands = run_emob_integration(edisgo_obj=edisgo_obj, save=True)
 
+# resample to 1h resolution
+logger.info("Resample to 1h resolution")
+edisgo_obj.resample_timeseries(method="ffill", freq="1h")
+
 # hp integration
+logger.info("Start heat pump integration")
 edisgo_obj = run_hp_integration(edisgo_obj=edisgo_obj, save=True)
 
 # bess integration
