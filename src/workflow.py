b6e2acd22b9f2d4606433cfd99e9734accb0025e
Julian.Endres <julian.endres@rl-institut.de>
2022-11-05 00:09:28 +0100

    Update workflow to changed doit style

--- src/workflow.py
+++ src/workflow.py
@@ -6,23 +6,24 @@ from emob_integration import run_emob_integration
 from hp_integration import run_hp_integration
 from load_integration import run_load_integration
 from feeder_extraction import run_feeder_extraction
+from dnm_generation import run_dnm_generation
+
 from logger import logger
 from tools import get_config, get_dir
 
 config_dir = get_dir(key="config")
 
-cfg = get_config(Path(f"{config_dir}/model_config.yaml"))
-multi_grid_ids = cfg["model"].get("multi_grid_ids")
+cfg = get_config(path=config_dir/"model_config.yaml")
+multi_grid_ids = cfg["grid_generation"].get("grids")
 
 for mvgd in multi_grid_ids:
+
     # load generation
-    logger.info(f"Start load integration for {mvgd}.")
-    edisgo_obj = run_load_integration(grid_id=mvgd, save=True)
+    edisgo_obj = run_load_integration(grid_id=mvgd, targets=True)
 
     # emob integration
-    logger.info(f"Start emob integration for {mvgd}.")
     edisgo_obj, flex_bands = run_emob_integration(
-        edisgo_obj=edisgo_obj, grid_id=mvgd, save=True, to_freq="1h"
+        edisgo_obj=edisgo_obj, grid_id=mvgd, targets=True, to_freq="1h"
     )
 
     # # resample to 1h resolution
@@ -30,13 +31,18 @@ for mvgd in multi_grid_ids:
     # edisgo_obj.resample_timeseries(method="ffill", freq="1h")
 
     # hp integration
-    logger.info(f"Start heat pump integration for {mvgd}.")
-    edisgo_obj = run_hp_integration(edisgo_obj=edisgo_obj, grid_id=mvgd, save=True)
+    edisgo_obj = run_hp_integration(edisgo_obj=edisgo_obj, grid_id=mvgd,
+                                    targets=True)
 
     # bess integration
     # residential mit pv_rooftop
 
     # feeder extraction
-    run_feeder_extraction()
+    feeders, buses_with_feeders = run_feeder_extraction(edisgo_obj=edisgo_obj,
+                                                      grid_id=mvgd, targets=True)
+
+    # generate downstream nodes matrix
+    run_dnm_generation(grid_id=mvgd, targets=True)
+
     # model optimization
     logger.info(f"MVGD: {mvgd} done.")
