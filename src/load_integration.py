e7ac934ab3b0e439f6f2d340ec4886bd8196c080
Julian.Endres <julian.endres@rl-institut.de>
2022-11-04 23:58:01 +0100

    Update load integration for doit

--- src/load_integration.py
+++ src/load_integration.py
@@ -4,13 +4,10 @@ from pathlib import Path
 
 import pandas as pd
 
-from edisgo.edisgo import EDisGo  # , import_edisgo_from_files
+from edisgo.edisgo import EDisGo
 
 from logger import logger
 from tools import get_config, get_dir, timeit
-
-# from edisgo.network.electromobility import get_energy_bands_for_optimization
-
 
 logs_dir = get_dir(key="logs")
 data_dir = get_dir(key="data")
@@ -18,16 +15,15 @@ config_dir = get_dir(key="config")
 
 
 @timeit
-def run_load_integration(edisgo_obj=False, grid_id=False, save=False):
+def run_load_integration(grid_id, edisgo_obj=False, targets=False,
+                         doit=False):
 
-    cfg = get_config(Path(f"{config_dir}/model_config.yaml"))
-
-    if not grid_id:
-        grid_id = cfg["model"].get("grid-id")
+    logger.info(f"Start load integration for {grid_id}.")
+    cfg = get_config(path=config_dir / "model_config.yaml")
 
     if not edisgo_obj:
 
-        import_dir = cfg["directories"]["load_integration"].get("import")
+        import_dir = cfg["grid_generation"]["load_integration"].get("import")
 
         ding0_grid = data_dir / import_dir / str(grid_id)
         edisgo_obj = EDisGo(ding0_grid=ding0_grid)
@@ -44,9 +40,18 @@ def run_load_integration(edisgo_obj=False, grid_id=False, save=False):
     )
     edisgo_obj.set_time_series_reactive_power_control()
 
-    if save:
-        export_dir = cfg["directories"]["load_integration"].get("export")
-        export_path = data_dir / export_dir / str(grid_id)
+    if targets:
+        if isinstance(targets, Path):
+            logger.debug("Use export dir given as parameter.")
+            export_path = targets
+        elif isinstance(targets, str):
+            logger.debug("Use export dir given as parameter.")
+            export_path = Path(targets)
+        else:
+            logger.debug("Use export dir from config file.")
+            export_dir = cfg["grid_generation"]["load_integration"].get("export")
+            export_path = data_dir / export_dir / str(grid_id)
+
         os.makedirs(export_path, exist_ok=True)
         edisgo_obj.save(
             export_path,
@@ -55,9 +60,12 @@ def run_load_integration(edisgo_obj=False, grid_id=False, save=False):
         )
         logger.info(f"Saved grid to {export_path}")
 
-    return edisgo_obj
+    if doit:
+        return True
+    else:
+        return edisgo_obj
 
 
 if __name__ == "__main__":
 
-    edisgo_obj = run_load_integration(save=True)
+    edisgo_obj = run_load_integration(grid_id=176)
