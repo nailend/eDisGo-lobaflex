19acf59911bea8fa9adcfc32ae81a87a732dbf17
Julian.Endres <julian.endres@rl-institut.de>
2022-11-11 21:49:30 +0100

    Rework opt script

--- src/model_solving.py
+++ src/model_solving.py
@@ -12,59 +12,57 @@ from edisgo.edisgo import import_edisgo_from_files
 from edisgo.tools.tools import convert_impedances_to_mv
 from loguru import logger
 
-from config import __path__ as config_dir
-from data import __path__ as data_dir
-from results import __path__ as results_dir
-from tools import get_config, setup_logfile
+from tools import get_config, get_dir
 
 # from edisgo.tools import logger
 
 
-data_dir = data_dir[0]
-results_dir = results_dir[0]
-config_dir = config_dir[0]
+config_dir = get_dir(key="config")
+data_dir = get_dir(key="data")
+results_dir = get_dir(key="results")
 
 
 # logger = logging.getLogger(__name__)
 
 
-def setup_directory(cfg_m):
-    working_dir = (
-        Path(cfg_m["working-dir"])
-        / Path(cfg_m["grid-id"])
-        / Path(cfg_m["feeder-id"])
-    )
-    os.makedirs(working_dir, exist_ok=True)
+def get_dnm(mvgd, feeder):
+    """Get Downstream Node Matrix that describes the influence of the nodes on
+    each other"""
+    cfd_g = get_config(path=config_dir / ".grids.yaml")
+    feeder_dir = cfd_g["dnm_generation"].get("import")
 
-
-def get_downstream_matrix(import_path, edisgo_obj):
-    """Matrix that describes the influence of the nodes on each other"""
-    cfg_m = get_config()["model"]
-    downstream_nodes_matrix = pd.read_csv(
-        os.path.join(
-            import_path,
-            f"downstream_node_matrix_{cfg_m['grid-id']}_{cfg_m['feeder-id']}.csv",
-        ),
-        index_col=0,
-    )
+    import_path = data_dir / feeder_dir / mvgd / "feeder" / feeder
+    file_path = import_path / f"downstream_node_matrix_{mvgd}" \
+                              f"_{feeder}.csv"
+    downstream_nodes_matrix = pd.read_csv(os.path.join(file_path), index_col=0)
     # TODO could be sparse?
     downstream_nodes_matrix = downstream_nodes_matrix.astype(np.uint8)
-    downstream_nodes_matrix = downstream_nodes_matrix.loc[
-        edisgo_obj.topology.buses_df.index, edisgo_obj.topology.buses_df.index
-    ]
+    # TODO warum hier loc? vermutlich weil anja nur eine downstream node
+    #  matrix für alle hatte
+    # downstream_nodes_matrix = downstream_nodes_matrix.loc[
+    #     edisgo_obj.topology.buses_df.index, edisgo_obj.topology.buses_df.index
+    # ]
     return downstream_nodes_matrix
 
 
-def run_optimization():
+# TODO
+#   rolling horizon
+#
+def run_optimization(grid_id, feeder_id=False, edisgo_obj=False,
+                     save=False, doit=False):
 
-    cfg = get_config(Path(f"{config_dir}/model_config.yaml"))
-    if not grid_id:
-        grid_id = cfg["model"].get("grid-id")
+    cfg_g = get_config(path=config_dir / ".grids.yaml")
+    cfg_o = get_config(path=config_dir / ".opt.yaml")
+    # mvgds = cfg_g["model"].get("mvgd")
 
     if not edisgo_obj:
+        if not feeder_id:
+            # TODO if feeder_id False
+            raise NotImplementedError
 
-        import_dir = cfg["directories"]["hp_integration"].get("import")
-        import_path = data_dir / import_dir / str(grid_id)
+        import_dir = cfg_g["feeder_extraction"].get("export")
+        import_path = data_dir / import_dir / str(grid_id) / "feeder" / str(
+            feeder_id)
         logger.info(f"Import Grid from file: {import_path}")
 
         edisgo_obj = import_edisgo_from_files(
@@ -75,108 +73,61 @@ def run_optimization():
             import_electromobility=True,
         )
 
-    cfg = get_config(Path(config_dir) / "model_config.yaml")
-    cfg_m = cfg["model"]
-    grid_id = cfg_m["grid-id"]
-    feeder_id = cfg_m["feeder-id"]
-    # setup_logfile(cfg)
-    # print = logger.info
-    # logger = logging.getLogger("pyomo")
-    # logger = logging.getLogger("pyomo.contrib.appsi.solvers.gurobi")
-    # logger.handlers.clear()
-    # console_handler = logging.StreamHandler(stream=sys.stdout)
-    # console_handler.setLevel(logging.INFO)
-    # stream_formatter = logging.Formatter("%(name)s - %(levelname)s: %(message)s")
-    # console_handler.setFormatter(stream_formatter)
-    # logger.addHandler(console_handler)
-    # logger.setLevel(logging.INFO)
-    # logger.propagate = False
-
-    # import Grid
-    # import_dir = Path(results_dir) / str(cfg_m["grid-id"]) / str(cfg_m["feeder-id"])
-    import_dir = os.path.join(
-        results_dir, f"edisgo_objects_emob_hp" f"/{grid_id}/{feeder_id}"
-    )
-
-    # TODO add import for heatpumps
-    edisgo_obj = import_edisgo_from_files(
-        import_dir, import_timeseries=True, import_heat_pump=True
-    )
+    # TODO temporary workaround
+    timesteps = list(range(24))
+    timesteps = edisgo_obj.timeseries.timeindex[timesteps]
 
     # Due to different voltage levels, impedances need to adapted
     # TODO alternatively p.u.
+    logger.info("Convert impedances to mv")
     edisgo_obj = convert_impedances_to_mv(edisgo_obj)
-    logger.info("Converted impedances to mv")
 
-    downstream_nodes_matrix = get_downstream_matrix(import_dir, edisgo_obj)
     logger.info("Downstream node matrix imported")
+    downstream_nodes_matrix = get_dnm(mvgd=grid_id, feeder=feeder_id)
 
+    logger.info("Extract time-invariant parameters")
     # Create dict with time invariant parameters
     parameters = lopf.prepare_time_invariant_parameters(
         edisgo_obj,
         downstream_nodes_matrix,
         pu=False,
-        optimize_storage=False,
-        optimize_ev_charging=False,
-        optimize_hp=True,
+        optimize_storage=cfg_o["opt_storage"],
+        optimize_ev_charging=cfg_o["opt_ev"],
+        optimize_hp=cfg_o["opt_hp"],
     )
-    logger.info("Time-invariant parameters extracted")
 
-    timesteps = list(range(24))
-    timesteps = edisgo_obj.timeseries.timeindex[timesteps]
-
-    # get break between weeks
-    # timesteps = pd.Series(edisgo_obj.timeseries.timeindex).diff().idxmax()
-    # timesteps = edisgo_obj.timeseries.timeindex[:timesteps]
+    logger.info("Setup model")
     model = lopf.setup_model(
         parameters,
         timesteps=timesteps,
-        objective="residual_load",
+        objective=cfg_o["objective"],
     )
-    logger.info("Model is setup")
 
     logger.info("Optimize model")
-    results = lopf.optimize(model, "gurobi")
+    results = lopf.optimize(model=model, solver=cfg_o["solver"])
+
+    export_path = results_dir / grid_id / feeder_id
+    os.makedirs(export_path, exist_ok=True)
+    logger.info(f"Export HP-results to: {export_path}")
 
     # Export HP-results
-    export_dir = Path(
-        f"{cfg_m['working-dir']}/{cfg_m['grid-id']}/{cfg_m['feeder-id']}"
-    )
     for res_name, res in results.items():
         try:
             res = res.loc[timesteps]
         except Exception:
+            # TODO handle this properly
+            logger.debug(f"Exception {res_name}")
             pass
         if "slack" in res_name:
-            res = res[res > 1e-6]
+            res = res[res > 1e-6] # TODO tolerance?
             res = res.dropna(how="all")
             res = res.dropna(how="all")
         if not res.empty:
-            res.astype(np.float16).to_csv(
-                export_dir
-                / Path("results/powerflow_results")
-                / Path(
-                    f"{res_name}_{cfg_m['grid-id']}_{cfg_m['feeder-id']}.csv"
-                )
-            )
-    print(f"Saved results to: {export_dir}/powerflow_results.")
-
-    # TODO return results for df
-    # TODO reinforce
-    # export reinforce results
-    # existing_loggers = [logging.getLogger()]  # get the root logger
-    # existing_loggers = existing_loggers + [
-    #     logging.getLogger(name) for name in logging.root.manager.loggerDict
-    # ]
-    #
-    # for logger in existing_loggers:
-    #     print(logger)
-    #     print(logger.handlers)
-    #     print("-"*20)
-    logger.info("Model solved")
+            filename = export_path / f"{res_name}.csv"
+            res.astype(np.float16).to_csv(path=filename)
 
     # TODO
-    # # TODO add emob ts to obj
+    # # TODO add results to obj
     # edisgo_obj.set_time_series_manual(
     #         loads_p=None,
     #         storage_units_p=None,
