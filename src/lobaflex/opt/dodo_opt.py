5038800e036429347a8137f5513ddfffe6cd438b
Julian.Endres <julian.endres@rl-institut.de>
2023-01-31 11:10:44 +0100

    Fix changes of concat input

--- src/lobaflex/opt/dodo_opt.py
+++ src/lobaflex/opt/dodo_opt.py
@@ -109,7 +109,7 @@ def task_concat_results():
                             save_concatinated_results,
                             [],
                             {
-                                "grids": mvgd,
+                                "grid_id": mvgd,
                                 "doit": True,
                                 "version": opt_version,
                             },
