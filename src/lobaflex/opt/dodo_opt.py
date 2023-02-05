051fd9c41e3dd3c9621a59ac18cedd4affad6e0c
Julian.Endres <julian.endres@rl-institut.de>
2023-02-05 14:38:44 +0100

    Finish pipeline for min reinforcement

--- src/lobaflex/opt/dodo_opt.py
+++ src/lobaflex/opt/dodo_opt.py
@@ -2,14 +2,21 @@ import logging
 import os
 
 from datetime import datetime
+
 import doit
 
+from doit import create_after
+
 from lobaflex import config_dir, data_dir, logs_dir, results_dir
-from lobaflex.opt.dispatch_optimization import run_dispatch_optimization
-from lobaflex.opt.result_concatination import save_concatinated_results
-from lobaflex.opt.minimal_reinforcement import integrate_and_reinforce
+from lobaflex.opt.tasks import (  # dnm_generation_task,
+    dispatch_integration_task,
+    feeder_extraction_task,
+    grid_reinforcement_task,
+    optimization_task,
+    result_concatination_task,
+    timeframe_selection_task,
+)
 from lobaflex.tools.logger import setup_logging
-from lobaflex.opt.tasks import *
 from lobaflex.tools.pydoit import (
     opt_uptodate,
     task__get_opt_version,
@@ -30,170 +37,109 @@ setup_logging(file_name=logfile)
 #   9. Check connection to db, maybe at beginning and raise warning
 
 DOIT_CONFIG = {
-    "default_tasks": ["opt", "concat_results", "min_reinforce"],
+    "default_tasks": ["prep", "min_load"],
     "reporter": TelegramReporter,
 }
 
-logger.info(f"Run: {cfg_o.get('run_id', None)} - Version:"
-            f"{cfg_o.get('version', None)}")
+logger.info(
+    f"Run: {cfg_o.get('run_id', None)} - Version:"
+    f"{cfg_o.get('version', None)}"
+)
+
+# DOIT_CONFIG = {
+#     "default_tasks": ["_set_opt_version"],
+#     "reporter": TelegramReporter,
+# }
+# DOIT_CONFIG = {
+#     "default_tasks": ["prep", "min_load"],
+#     # "reporter": TelegramReporter,
+# }
 
 
-def task_opt():
+def task_prep():
+    """Prepare rolling horizon optimization
+    Feeder extraction and dnm matrix generation"""
+
+    cfg_o = get_config(path=config_dir / ".opt.yaml")
+    mvgds = sorted(cfg_o["mvgds"])
+    import_dir = cfg_o["import_dir"]
+
+    dep_manager = doit.Globals.dep_manager
+    version = dep_manager.get_result("_set_opt_version")["version"]
+    run_id = dep_manager.get_result("_set_opt_version")["run_id"]
+
+    # TODO
+    # observattion_import = data_dir / import_dir / str(mvgd)
+    # path = results_dir / run_id / str(mvgd) / timeframe
+    for mvgd in mvgds:
+        data_path = data_dir / import_dir / str(mvgd)
+        if os.path.isdir(data_path):
+
+            # TODO observation periods
+            yield timeframe_selection_task(mvgd, run_id, version)
+
+            yield feeder_extraction_task(mvgd, run_id, version)
+
+            # yield dnm_generation_task(mvgd=mvgd,
+            #                           run_id=run_id,
+            #                           version=version,)
+
+
+@create_after(executed="prep")
+def task_min_load():
     """Generator for optimization tasks"""
 
     cfg_o = get_config(path=config_dir / ".opt.yaml")
     mvgds = sorted(cfg_o["mvgds"])
-    feeder_dir = cfg_o["import_dir"]
+    # import_dir = cfg_o["import_dir"]
+    # objective = cfg_o["objective"]
+    objective = "minimize_loading"
+    # feeder_dir =
     dep_manager = doit.Globals.dep_manager
-    opt_version = dep_manager.get_result("_set_opt_version")["version"]
+    version = dep_manager.get_result("_set_opt_version")["version"]
+    run_id = dep_manager.get_result("_set_opt_version")["run_id"]
 
     # create opt task only for existing grid folders
     for mvgd in mvgds:
-        feeder_path = data_dir / feeder_dir / str(mvgd) / "feeder"
-        try:
+        if os.path.isdir(results_dir / run_id / str(mvgd)):
+
+            mvgd_path = results_dir / run_id / str(mvgd)
+            feeder_path = mvgd_path / "timeframe_feeder"
+
             feeder_ids = [
-                feeder_id
-                for feeder_id in os.listdir(feeder_path)
-                if os.path.isdir(feeder_path / feeder_id)
+                f
+                for f in os.listdir(feeder_path)
+                if os.path.isdir(feeder_path / f)
             ]
-        except FileNotFoundError as e:
-            logger.info(f"No Files found for MVGD: {mvgd}")
-            continue
-        for feeder in sorted(feeder_ids):
-            yield {
-                "name": f"{mvgd}/{int(feeder):02}",
-                "actions": [
-                    (
-                        run_dispatch_optimization,
-                        [],
-                        {
-                            "grid_id": mvgd,
-                            "feeder_id": feeder,
-                            "doit": True,
-                            "save": True,
-                            "version": opt_version,
-                        },
-                    )
-                ],
-                # "task_dep": [f"grids:{mvgd}_feeder_extraction"],
-                # "getargs": {"version": ("_get_opt_version", "version")},
-                "uptodate": [opt_uptodate],
-                "verbosity": 2,
-            }
 
+            dependencies = []
+            for feeder in sorted(feeder_ids):
 
-def task_concat_results():
-    """Generator for concatination tasks"""
-
-    cfg_o = get_config(path=config_dir / ".opt.yaml")
-    mvgds = sorted(cfg_o["mvgds"])
-    feeder_dir = cfg_o["import_dir"]
-    dep_manager = doit.Globals.dep_manager
-    opt_version = dep_manager.get_result("_set_opt_version")["version"]
-
-    # create opt task only for existing grid folders
-    # also used for dependency
-    for mvgd in mvgds:
-        mvgd_path = data_dir / feeder_dir / str(mvgd)
-        if os.path.isdir(mvgd_path):
-            feeder_path = data_dir / feeder_dir / str(mvgd) / "feeder"
-            try:
-                feeder_ids = [
-                    feeder_id
-                    for feeder_id in os.listdir(feeder_path)
-                    if os.path.isdir(feeder_path / feeder_id)
-                ]
-                yield {
-                    "name": f"{mvgd}",
-                    "actions": [
-                        (
-                            save_concatinated_results,
-                            [],
-                            {
-                                "grid_id": mvgd,
-                                "doit": True,
-                                "version": opt_version,
-                            },
-                        )
-                    ],
-                    "doc": "per mvgd",
-                    # create dependency for every feeder in grid not only opt
-                    # results if not all opt succeeded
-                    "task_dep": [
-                        f"opt:{mvgd}/{int(feeder):02}"
-                        for feeder in feeder_ids
-                    ],
-                    # "getargs": {"version": ("_get_opt_version", "version")},
-                    "uptodate": [opt_uptodate],
-                }
-            except FileNotFoundError as e:
-                logger.info(
-                    f"No Files found for concat dependency of MVGD: {mvgd}."
+                yield optimization_task(
+                    mvgd, feeder, objective, run_id, version
                 )
-                continue
-                
 
-def task_min_reinforce():
-    """Generator for minimal reinforcement tasks"""
+                dependencies += [f"min_load:opt_{mvgd}/{int(feeder):02}"]
 
-    cfg_o = get_config(path=config_dir / ".opt.yaml")
-    mvgds = sorted(cfg_o["mvgds"])
-    feeder_dir = cfg_o["import_dir"]
-    dep_manager = doit.Globals.dep_manager
-    opt_version = dep_manager.get_result("_set_opt_version")["version"]
+            yield result_concatination_task(
+                mvgd, objective, run_id, version, dep=dependencies
+            )
 
-    # create opt task only for existing grid folders
-    for mvgd in mvgds:
-        mvgd_path = data_dir / feeder_dir / str(mvgd)
-        if os.path.isdir(mvgd_path):
-            yield {
-                "name": f"{mvgd}",
-                "actions": [
-                    (
-                        integrate_and_reinforce,
-                        [],
-                        {
-                            "grid_id": mvgd,
-                            "doit": True,
-                            "version": opt_version,
-                        },
-                    )
-                ],
-                "doc": "per mvgd",
-                "task_dep": [f"concat_results:{mvgd}"],
-                # "getargs": {"version": ("_get_opt_version", "version")},
-                "uptodate": [opt_uptodate],
-            }
-        else:
-            logger.info(f"No Files found for result integration of {mvgd}.")
+            yield dispatch_integration_task(
+                mvgd,
+                objective,
+                run_id,
+                version,
+                dep=[f"min_load:concat_{mvgd}"],
+            )
 
-
-def task_opt_group():
-    """Group opt tasks"""
-    cfg_o = get_config(path=config_dir / ".opt.yaml")
-    mvgds = sorted(cfg_o.get("mvgds"))
-
-    cfd_g = get_config(path=config_dir / ".grids.yaml")
-    feeder_dir = cfd_g["feeder_extraction"].get("export")
-
-    for mvgd in mvgds:
-        feeder_path = data_dir / feeder_dir / str(mvgd) / "feeder"
-        try:
-            feeder_ids = [
-                feeder_id
-                for feeder_id in sorted(os.listdir(feeder_path))
-                if os.path.isdir(feeder_path / feeder_id)
-            ]
-        except FileNotFoundError as e:
-            continue
-        yield {
-            "actions": None,
-            "name": str(mvgd),
-            "doc": "per mvgd",
-            "task_dep": [
-                f"opt:{mvgd}/{int(i):02}" for i in feeder_ids
-            ],
-        }
+            yield grid_reinforcement_task(
+                mvgd,
+                objective,
+                run_id,
+                version,
+                dep=[f"min_load:add_ts_{mvgd}"],
+            )
 
 
 if __name__ == "__main__":
