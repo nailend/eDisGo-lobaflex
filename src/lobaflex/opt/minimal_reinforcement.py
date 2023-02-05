28fe979f79027c58d609f75e379fab671e2cd1e8
Julian.Endres <julian.endres@rl-institut.de>
2023-02-05 11:26:26 +0100

    Adjust reinforcement to new workflow

--- src/lobaflex/opt/minimal_reinforcement.py
+++ src/lobaflex/opt/minimal_reinforcement.py
@@ -5,18 +5,15 @@ import warnings
 
 from copy import deepcopy
 from datetime import datetime
-from functools import partial
 
 import numpy as np
 import pandas as pd
 
-from edisgo.edisgo import import_edisgo_from_files
+from edisgo.edisgo import EDisGo, import_edisgo_from_files
 
-from lobaflex import config_dir, data_dir, logs_dir, results_dir
-from lobaflex.grids.feeder_extraction import get_flexible_loads
-from lobaflex.opt.dispatch_optimization import extract_timeframe
+from lobaflex import config_dir, logs_dir, results_dir
 from lobaflex.tools.logger import setup_logging
-from lobaflex.tools.tools import get_config, get_files_in_subdirs, log_errors
+from lobaflex.tools.tools import get_config, log_errors
 
 if __name__ == "__main__":
     logger = logging.getLogger("lobaflex.opt." + __name__)
@@ -24,100 +21,14 @@ else:
     logger = logging.getLogger(__name__)
 
 
-def integrate_opt_results(
-    edisgo_obj,
-    parameters,
-    start_datetime=None,
-    periods=None,
-    run_id=None,
-    grid_id=None,
+def iterative_reinforce(
+    edisgo_obj, mode=None, iterations=10, combined_analysis=False
 ):
-    """
-
-    Parameters
-    ----------
-    edisgo_obj :
-    parameters :
-    start_datetime :
-    periods :
-    run_id :
-    grid_id :
-
-    Returns
-    -------
-
-    """
-    edisgo_obj = deepcopy(edisgo_obj)
-
-    cfg_o = get_config(path=config_dir / ".opt.yaml")
-    if grid_id is None:
-        grid_id = edisgo_obj.topology.mv_grid.id
-    if run_id is None:
-        run_id = cfg_o["run_id"]
-
-    logger.info(f"Start result integration of {grid_id} in {run_id}.")
-
-    results_path = results_dir / run_id / str(grid_id) / "mvgd"
-
-    list_of_files = get_files_in_subdirs(results_path, pattern="*.csv")
-
-    filenames = [
-        file
-        for file in list_of_files
-        for parameter in parameters
-        if parameter in file
-    ]
-
-    logger.info(f"Import results of {' & '.join(parameters)}.")
-    df_loads_active_power = pd.concat(
-        map(partial(pd.read_csv, index_col=0, parse_dates=True), filenames),
-        axis=1,
-    )
-
-    # mark all optimised loads
-    edisgo_obj.topology.loads_df.loc[:, "opt"] = False
-    edisgo_obj.topology.loads_df.loc[
-        df_loads_active_power.columns, "opt"
-    ] = True
-    # define timeframe to concat
-    if start_datetime and periods is not None:
-        timeframe = pd.date_range(
-            start=cfg_o["start_datetime"],
-            periods=cfg_o["total_timesteps"],
-            freq="1h",
-        )
-        df_loads_active_power = df_loads_active_power.loc[timeframe]
-    else:
-        timeframe = df_loads_active_power.index
-
-    logger.info("Reduce timeseries to selected timeframe.")
-    edisgo_obj = extract_timeframe(edisgo_obj=edisgo_obj, timeframe=timeframe)
-
-    logger.info("Drop former timeseries of flexible loads.")
-    edisgo_obj.timeseries.loads_active_power = (
-        edisgo_obj.timeseries.loads_active_power.drop(
-            columns=df_loads_active_power.columns, errors="ignore"
-        )
-    )
-
-    logger.info("Concatinate optimized loads.")
-    edisgo_obj.timeseries.loads_active_power = pd.concat(
-        [edisgo_obj.timeseries.loads_active_power, df_loads_active_power],
-        axis=1,
-    )
-
-    logger.info("Set reactive power")
-    edisgo_obj.set_time_series_reactive_power_control()
-
-    return edisgo_obj
-
-
-def reinforce(edisgo_obj, mode=None, iterations=10, combined_analysis=False):
     """Edisgo reinforce is conducted if Value Error is raised.
 
     Parameters
     ----------
-    edisgo_obj : :class:`~.EDisGo`
+    edisgo_obj : :class:`edisgo.EDisGo`
         EDisGo object
     mode : str
             * 'split'
@@ -180,7 +91,8 @@ def reinforce(edisgo_obj, mode=None, iterations=10, combined_analysis=False):
             edisgo_obj.reinforce(
                 reduced_timesteps,
                 combined_analysis=combined_analysis,
-                troubleshooting_mode="lpf",
+                # troubleshooting_mode="lpf", # said to be only useful in
+                # case of non-convergence, mostly bad seed
                 raise_not_converged=False,
                 max_while_iterations=50,
             )
@@ -191,7 +103,7 @@ def reinforce(edisgo_obj, mode=None, iterations=10, combined_analysis=False):
             edisgo_obj.reinforce(
                 exluded_timesteps,
                 combined_analysis=combined_analysis,
-                troubleshooting_mode="lpf",
+                # troubleshooting_mode="lpf",
                 raise_not_converge=False,
                 max_while_iterations=50,
             )
@@ -233,16 +145,14 @@ def reinforce(edisgo_obj, mode=None, iterations=10, combined_analysis=False):
 
 
 @log_errors
-def integrate_and_reinforce(
-    edisgo_obj=None, grid_id=None, doit=False, version=None
-):
+def reinforce_grid(obj_or_path, grid_id=None, run_id=None, version=None):
     """
 
     Parameters
     ----------
-    edisgo_obj :
+    obj_or_path :
     grid_id :
-    doit :
+    run_id :
     version :
 
     Returns
@@ -251,52 +161,30 @@ def integrate_and_reinforce(
     """
     warnings.simplefilter(action="ignore", category=FutureWarning)
 
-    cfg_o = get_config(path=config_dir / ".opt.yaml")
-    cfg_g = get_config(path=config_dir / ".grids.yaml")
-    run_id = cfg_o["run_id"]
-
     logger.info(f"Start integrate and reinforce of {grid_id} in {run_id}.")
 
     date = datetime.now().date().isoformat()
     logfile = logs_dir / f"opt_minimal_reinforcement_{run_id}_{date}.log"
     setup_logging(file_name=logfile)
 
-    date = datetime.now().date().isoformat()
-    logfile = logs_dir / f"reinforcement_{grid_id}_{date}.log"
-    setup_logging(file_name=logfile)
-
-    if edisgo_obj and grid_id is None:
-        raise ValueError("Either edisgo_object or grid_id has to be given.")
-    if edisgo_obj is None:
-        import_dir = cfg_g["feeder_extraction"].get("import")
-        import_path = data_dir / import_dir / str(grid_id)
-        logger.info(f"Import Grid from file: {import_path}")
+    if isinstance(obj_or_path, EDisGo):
+        edisgo_obj = obj_or_path
+    else:
+        logger.info(f"Import Grid from file: {obj_or_path}")
 
         edisgo_obj = import_edisgo_from_files(
-            import_path,
+            obj_or_path,
             import_topology=True,
             import_timeseries=True,
             import_electromobility=True,
             import_heat_pump=True,
         )
 
-    # TODO define via config
-    selected_parameters = []
-    if cfg_o["opt_hp"]:
-        selected_parameters += ["charging_hp_el"]
-    if cfg_o["opt_emob"]:
-        selected_parameters += ["charging_ev"]
-
-    logger.info(f"Selected parameters: {'& '.join(selected_parameters)}")
-    edisgo_obj = integrate_opt_results(
-        edisgo_obj, parameters=selected_parameters
-    )
-
-    edisgo_obj = reinforce(
+    edisgo_obj = iterative_reinforce(
         edisgo_obj, combined_analysis=True, mode="split", iterations=5
     )
 
-    export_path = results_dir / run_id / str(grid_id) / "min_reinforce"
+    export_path = results_dir / run_id / str(grid_id) / "min_reinforced"
     os.makedirs(export_path, exist_ok=True)
 
     logger.info("Save reinforced grid")
@@ -309,7 +197,7 @@ def integrate_and_reinforce(
         save_results=True,
     )
 
-    if doit:
+    if version is not None and run_id is not None:
         return {"version": version, "run_id": run_id}
 
 
@@ -325,13 +213,9 @@ if __name__ == "__main__":
     logfile = logs_dir / f"minimal_reinforcement_{date}_local.log"
     setup_logging(file_name=logfile)
 
-    integrate_and_reinforce(
-        # grid_id=1056, feeder_id=1, edisgo_obj=False, save=True, doit=False
-        # grid_id=176,
-        # grid_id=1056,
+    reinforce_grid(
+        obj_or_path=results_dir / "debug" / "1111" / "minimize_loading_mvgd",
         grid_id=1111,
-        # feeder_id=8,
-        doit=False,
+        run_id=None,
+        version=None,
     )
-
-    # lopf.combine_results_for_grid
